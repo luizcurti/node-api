c6a7c1f51d0792c1cc461d46c5ca2dce8a13affa
Luiz <luiz_curti@hotmail.com>
2021-03-21 22:51:27 +0000

    Estudos
    Estudos

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
 requireDir('./src/models');
